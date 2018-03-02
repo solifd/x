4fbea1e08ca26af59c66176288446d3469ddc702
solifd <sgovep@gmail.com>
2018-03-02 17:19:51 +0800

    Update adblock.sh

--- adblock.sh
+++ adblock.sh
@@ -68,3 +68,8 @@ systemctl restart unbound.service > /dev/null 2>&1
 
 # Confirm completion
 echo "Installed blocklist and set to update daily"
+sed -i /googleadservices/d /etc/unbound/local.d/adaway.conf
+sed -i /googletagservices/d /etc/unbound/local.d/adaway.conf
+sed -i /googleadsserving/d /etc/unbound/local.d/adaway.conf
+sed -i /googlesyndication/d /etc/unbound/local.d/adaway.conf
+sed -i /google-analytics/d /etc/unbound/local.d/adaway.conf
